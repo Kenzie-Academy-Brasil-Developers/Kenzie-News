8dff2e6f0b8bab2c485f1ea31db6c855124c7291
Breno Leite <brenoleitepereira@gmail.com>
2022-08-23 09:47:18 -0300

    Temp: Carrossel adicionado, projeto aguardando correção

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ class KenzieNewsApi {
     const data = await fetch(`${kenzieUri}`)
       .then((res) => res.json())
       .then((res) => {
+        CriarCards.bannerCards(res);
         CriarCards.createCards(res);
       });
     return data;
@@ -12,15 +13,51 @@ class KenzieNewsApi {
 KenzieNewsApi.noticias();
 
 class CriarCards {
+  static bannerCards(noticia) {
+    const secaoInfo = document.createElement("section");
+    const main = document.querySelector("main");
+
+    let divImg = document.createElement("div");
+    let divTextos = document.createElement("div");
+    let figCarrossel = document.createElement("img");
+    let fonte = document.createElement("p");
+    let tagH1 = document.createElement("h1");
+    let categoria = document.createElement("span");
+
+
+    secaoInfo.classList.add("secaoBanner");
+    divTextos.classList.add("divTextos__banner");
+    divImg.classList.add("divImg__banner");
+
+
+    const findImg = noticia.find((elem) => {
+      if (elem.titulo.includes("Silmara")) {
+        figCarrossel.src = elem.imagem;
+        categoria.innerText = elem.categoria;
+        tagH1.innerText = elem.titulo;
+        fonte.innerText = `Fonte: ${elem.fonte}`;
+      }
+    });
+
+    divImg.append(figCarrossel)
+    divTextos.append(categoria, tagH1, fonte)
+    secaoInfo.append(divImg, divTextos);
+    main.append(secaoInfo);
+    return secaoInfo;
+  }
+
   static createCards(noticia) {
     const secaoInfo = document.createElement("section");
     const main = document.querySelector("main");
+
+    secaoInfo.classList.add("secaoNews");
+
 
     const noticiaPercorrida = noticia.forEach((elem, inx, arr) => {
       let div = document.createElement("div");
       let divImg = document.createElement("div");
       let divTextos = document.createElement("div");
-      
+
       let img = document.createElement("img");
       let categoria = document.createElement("span");
       let tagH1 = document.createElement("h1");
@@ -35,8 +72,8 @@ class CriarCards {
       categoria.innerText = elem.categoria;
       tagH1.innerText = elem.titulo;
       resumo.innerText = elem.resumo;
-      fonte.innerText = `Fonte: ${elem.fonte}`;      
-      
+      fonte.innerText = `Fonte: ${elem.fonte}`;
+
       divTextos.append(categoria, tagH1, resumo, fonte);
       divImg.append(img);
       div.append(divImg, divTextos);
@@ -45,7 +82,7 @@ class CriarCards {
       return secaoInfo;
     });
   }
-
 }
+CriarCards.bannerCards();
 CriarCards.createCards();
-// CriarCards.NoticiaPrincipal();
+// CriarCards.bannerCards();
